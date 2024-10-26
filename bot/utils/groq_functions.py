1c4a744bb33ae008cd2ffaab903c0e61dec8bf38
Ivan Filippov <filivandev@gmail.com>
2024-10-26 23:01:13 +1200

    Add docstring in utils

--- bot/utils/groq_functions.py
+++ bot/utils/groq_functions.py
@@ -11,6 +11,20 @@ chat_completion_model = "llama-3.2-90b-vision-preview"
 
 
 async def transcribe_voice(file_io: io.BytesIO) -> str:
+    """
+    Transcribes an audio file using the Groq API.
+
+    This asynchronous function takes an audio file in binary format and sends it
+    to the Groq API for transcription using the specified speech model.
+    If successful, it returns the transcribed text in Russian. If an error occurs
+    during the API call, it logs the exception and returns an error message.
+
+    Args:
+        file_io (io.BytesIO): The audio file in binary format to be transcribed.
+
+    Returns:
+        str: The transcribed text if successful, or an error message if an exception occurs.
+    """
     logger.info("Send binary-voice file to Groq {model} model", model=speech_model)
     try:
         transcription = await client.audio.transcriptions.create(
@@ -42,6 +56,20 @@ async def translate_to_english(file_io: io.BytesIO) -> str:
 
 
 async def chat_completion(text: str) -> str:
+    """
+    Completes a chat using the Groq API.
+
+    This asynchronous function takes a string of text and sends it to the Groq API
+    for chat completion using the specified chat completion model.
+    If successful, it returns the completed text. If an error occurs during the API
+    call, it logs the exception and returns an error message.
+
+    Args:
+        text (str): The text to be completed.
+
+    Returns:
+        str: The completed text if successful, or an error message if an exception occurs.
+    """
     logger.info(
         "Send text to Groq chat completion {model} model", model=chat_completion_model
     )
