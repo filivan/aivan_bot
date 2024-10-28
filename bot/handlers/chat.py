29cbfd16918836f8e0cb69cee223de3c590b5f69
Ivan Filippov <filivandev@gmail.com>
2024-10-29 00:02:51 +1200

    Return parsemode

--- bot/handlers/chat.py
+++ bot/handlers/chat.py
@@ -1,5 +1,6 @@
 import telegramify_markdown
 from aiogram import Router, F
+from aiogram.enums import ParseMode
 from aiogram.types import Message
 from loguru import logger
 from filters import MessageToBotFilter
@@ -19,9 +20,9 @@ async def message_to_text_with_completion(message: Message):
             else message.text[len(f"@{message.bot._me.username}") :]
         )
         completion = telegramify_markdown.markdownify(completion)
-        await message.reply(f"{completion}")
+        await message.reply(f"{completion}", parse_mode=ParseMode.MARKDOWN_V2)
         logger.info("Completion finished")
 
-    except Exception:
-        logger.exception("An error occurred")
-        await message.reply("An error occurred")
+    except Exception as e:
+        logger.exception(f"An error occurred")
+        await message.reply(f"An error occurred")
