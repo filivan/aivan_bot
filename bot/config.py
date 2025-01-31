7e2432304cd5bd99432361a74b6ceb356cf04874
Ivan Filippov <filivandev@gmail.com>
2025-02-01 00:02:57 +1200

    Change chat completion model to llama-3.3

--- bot/config.py
+++ bot/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     GROQ_API_KEY: str
     BOT_TOKEN: str
     ACCESS_PASSWORD: str
-    CHAT_COMPLETION_MODEL: str = "deepseek-r1-distill-llama-70b"
+    CHAT_COMPLETION_MODEL: str = "llama-3.3-70b-specdec" # "deepseek-r1-distill-llama-70b"
     SPEECH_MODEL: str = "whisper-large-v3-turbo"
     VISION_MODEL: str = "llama-3.2-90b-vision-preview" 
     AUTHORIZED_USERS_ID: set = {450267784}
