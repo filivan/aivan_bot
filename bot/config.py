8f17b6ca02b0640af6eba122390bcb3ab736c958
Ivan Filippov <filivandev@gmail.com>
2025-10-23 03:49:12 +1200

    Update models to newer ones. Refactor code structure for improved readability and maintainability.

--- bot/config.py
+++ bot/config.py
@@ -11,9 +11,11 @@ class Settings(BaseSettings):
     GROQ_API_KEY: str
     BOT_TOKEN: str
     ACCESS_PASSWORD: str
-    CHAT_COMPLETION_MODEL: str = "qwen-qwq-32b"  # "llama-3.3-70b-specdec"
-    SPEECH_MODEL: str = "whisper-large-v3-turbo"
-    VISION_MODEL: str = "llama-3.2-90b-vision-preview"
+    CHAT_COMPLETION_MODEL: str = "meta-llama/llama-4-maverick-17b-128e-instruct"
+    SPEECH_TEXT_MODEL: str = "whisper-large-v3-turbo"
+    TEXT_SPEECH_MODEL: str = "playai-tts"
+    TEXT_SPEECH_MODEL_VOICE: str = "Atlas-PlayAI"
+    VISION_MODEL: str = "meta-llama/llama-4-scout-17b-16e-instruct"
     AUTHORIZED_USERS_ID: set = {450267784}
     AUTHORIZED_CHATS_ID: set = {-1002263878476}
 
